fa139d7475f20b7c4136c1f2230b3dbd1d51850d
Kevin Lamping <kevin.lamping@usaa.com>
2012-03-28 14:43:41 -0500

    Kevin's Updates

--- greeter.js
+++ greeter.js
@@ -1,5 +1,7 @@
-YUI.add('greeter', function (Y) {
-    Y.greet = function (cfg) {
-        alert("Hello " + cfg.name);
+YUI.add('Greeter_js', function (Y) {
+    Y.Greet = function (cfg) {
+        Y.one(cfg.srcNode).on('click', function (ev) {
+            alert("Hello " + cfg.name);
+        });
     };
-});
+}, '1.0', {requires:['node', 'event', 'Greeter_css']});
